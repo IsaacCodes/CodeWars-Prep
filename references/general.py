5e770b2361cbec1f452735e00a5f1012c679859b
IsaacCodes <179248168+IsaacCodes@users.noreply.github.com>
2025-02-26 02:30:41 +0000

    Clarified + updated parts of references/general.py

--- references/general.py
+++ references/general.py
@@ -1,7 +1,7 @@
+#This "program" is not intended to be run
+#It is just for copy pasting code snippets
 from sys import exit
-if 1 == 1:
-  exit()
-#This "program" is not intended to be run. It is just for copy pasting code snippets
+exit()
 
 
 #Alphabet
@@ -10,30 +10,35 @@ alpha = "abcdefghijklmnopqrstuvwxyz"
 import string #Also contains other useful strings
 print(string.ascii_lowercase)
 
+
 #Switch rows + columns (transpose 2d list)
 #Rows and columns must be of same len, otherwise lowest len is used
 list1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
 list2 = [list(elem) for elem in list(zip(*list1))]
+
 #Use itertools.zip_longest to take longest len instead of shortest.
 from itertools import zip_longest
 list3 = [[1, 2], [4, 5, 6], [7, 8], [10, 11, 12]]
 list4 = [list(elem) for elem in list(zip_longest(*list3, fillvalue=-1))]
 
 
-#Increase recursion limit, 1000 by defualt
+#Increase recursion limit (1000 by defualt)
 from sys import setrecursionlimit
 setrecursionlimit(1500)
 
-#Pair of lists to dict, extra items ignored
+
+#Convert lists to dicts using zip or comprehension
 keys = ["a", "b", "c"]
 values = ["one", "two", "three"]
-dict = dict(zip(keys, values))
+dict1 = dict(zip(keys, values))
+dict2 = {keys[i] : values[i] for i in range(len(keys))}
 
-#Custom sort, ex. sort int-strings by magnitude
+
+#Custom sorting (ex. sort integer-strings by magnitude)
 list1 = ["1", "34", "-100", "900", "5", "-343"]
 sorted(list1, key=lambda x: abs(int(x)))
 
-#Custom sort using comparison not value function
+#Custom sorting using a comparison instead of value function
 #Useful in certain cases with tie breakers or not easily numericized values
 from functools import cmp_to_key
 list1 = ["1", "34", "-100", "900", "5", "-343"]
@@ -47,29 +52,33 @@ def comparison(v1, v2):
     return 0
 list2 = sorted(list1, key=cmp_to_key(comparison))
 
-#Anagram (same contents for both lists/strings)
+
+#Converts lists and strings to a dictionary-like counter
 from collections import Counter
 first = "abcd"
 second = "acdb"
+#For isntace, checking for anagrams (all the same elements + counts)
 is_anagram = Counter(first) == Counter(second)
 
-#All unique
+
+#Checks if all list items are unqiue
 list1 = [1, 2, 3, 4, 4, 5]
 all_unqiue = len(list1) == len(set(list1))
 
-#Exponential growth / compound intrest formula
-#Base number, intrest rate, times compounded, compounds per time
-p, r, t, n = 9035, 0.31, 10, 1
-a = p * (1 + r/n)**(n*t)
 
-#Convert bases, 2 digit identifier added to start
+#Convert decimal to binary, octal, or hexadecimal
+#String outputs are prefixed with 2 digit identifiers
 n = 1000
-bin_n = bin(n)
-hex_n = hex(n)
-oct_n = oct(n)
-dup_n = int(bin_n[2:], 2)
+bin_n = bin(n) # '0b1111101000'
+oct_n = oct(n) # '0o1750'
+hex_n = hex(n) # '0x3e8'
 
-#Convert to custom base (output as a list), ignores signs
+#Convert to base 10 from base b
+b = 16
+dec_n = int("3e8", b) # 1000
+
+#Convert to a custom base output as a list ignorning sign
+#Numbers can then be replaced as nessecary (ex. 11 -> "A")
 def n_to_base(n, base):
   n = abs(n)
   if n == 0:
@@ -82,12 +91,14 @@ def n_to_base(n, base):
   
   return digits[::-1]
 
-#Zfill right side
-def zfillright(n, zeros):
-  return str(n).split(".")[0] + "." + str(n).split(".")[1].ljust(zeros, "0")
-zfillright(1.2, 2)
-#Left zfill
-str(34).zfill(4)
+
+n = 3
+#Fill the number with 0's to the left until n digits
+print("12".zfill(3)) # '012'
+
+#Fill the number with 0's to the right after the decimal place until n digits
+print(f"{12:.{n}f}") # '12.000'
+
 
 #Date conversion from string to datetime object
 # %Y is year, %m is month, %d is day
